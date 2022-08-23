e59817d3c9e17e1ee2dd220ae3c170c2bc481947
DemonOrtiz9444 <ortizdevelop@gmail.com>
2022-08-23 13:59:17 +0300

    Finished working on the "header" block and the burger-menu button.
    html - The "header" block has been redesigned. Additional content, new classes and tags were added.
    
    css - Added scss/css style properties for header block, its classes and tags. Animation for burger menu (keyframes) was fully animated. Flex-elements in the code were used.
    
    assets\header, the content for the burger-menu was added to the folder assets\header.
    
    js - the function "hb.addEventListener" was added to create the animation of the burger-menu. When clicked, it activates the animation and blocks the surrounding background, allowing you to scroll through the menu separately.
    
    @AidenOrtiz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+
+const hb = document.querySelector(".header__hamburger");
+const menu = document.querySelector(".header");
+const body = document.querySelector("body");
+
+hb.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    hb.classList.toggle('active');
+    body.classList.toggle('lock');
+})
